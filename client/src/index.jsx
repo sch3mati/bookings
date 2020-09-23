a01f06be4b0d28cdf2ee9154277d66e602f61cfb
Johnny Tang <email.john.e@gmail.com>
2020-09-23 16:32:13 -0700

    configured webpack, initiated server to connect with react

---
+++ client/src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Bookings extends React.Component {
+  render() {
+    return (
+      <h1>Booking feature from React</h1>
+    )
+  }
+}
+
+ReactDOM.render(< Bookings />, document.getElementById('bookings'))
