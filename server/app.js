6f2e0fc80f30967edf3efef17265e908bce2c2fd
Victoria Chen <vicchen12@gmail.com>
2020-10-23 16:37:55 -0700

    connect newrelics and removed unecessary console.log

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+// const newrelic = require('newrelic');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
@@ -32,7 +33,6 @@ app.post('/api/restaurants/:restaurantId/bookings', (req, res) => {
 // app.post('/api/bookings/:restaurantId', (req, res) => {
   const reservation = req.body;
   reservation.restaurantId = req.params.restaurantId;
-  console.log(reservation);
   db.addReservation(reservation, (err) => {
     if (err) {
       res.status(400).send('could not reserve');
@@ -44,16 +44,12 @@ app.post('/api/restaurants/:restaurantId/bookings', (req, res) => {
 
 app.get('/api/restaurants/:restaurantId/bookings/:bookingId', (req, res) => {
 // app.get('/api/bookings/:restaurantId', (req, res) => {
-  console.log(req, 'req')
   const reservation = req.query;
-  console.log(reservation)
   reservation.restaurantId = req.params.restaurantId;
   db.getReservations(reservation, (err, reservationData, restaurantData) => {
     if (err) {
       res.status(400).send('error finding reservations');
     } else {
-      console.log('res', reservationData)
-      console.log('restar', restaurantData)
       res.status(200).send(calculateReservations(reservationData, restaurantData, reservation));
     }
   });
